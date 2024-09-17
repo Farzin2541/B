1e4e3069e09444c677b48641bb8f00edbc43b13a
Farzin <farzin2541@gmail.com>
2024-09-18 03:58:18 +0400

    tests

---
+++ run1.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+clear;
+loop=${1:-1000}
+waittime=${2:-3}
+for i in `seq 1 $loop`;
+do
+    echo "Running C++: $i out of $loop";
+    ./app1cpp;
+    if [ "$i" -lt "$loop" ]; then
+        sleep $waittime;
+    fi
+done
